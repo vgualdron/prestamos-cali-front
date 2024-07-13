09ad730153bcb3763e1af6605e910704c552376a
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-07-13 15:08:58 -0500

    push

--- quasar.conf.js
+++ quasar.conf.js
@@ -125,10 +125,9 @@ module.exports = function (ctx) {
 
     // https://v1.quasar.dev/quasar-cli/developing-pwa/configuring-pwa
     pwa: {
-      workboxPluginMode: 'InjectManifest', // 'GenerateSW' or 'InjectManifest'
+      workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {
-        swSrc: 'public/firebase-messaging-sw.js', // Asegúrate de que la ruta sea correcta
-        swDest: 'firebase-messaging-sw.js'
+        skipWaiting: true,
       }, // only for GenerateSW
       manifest: {
         name: 'Control de Prestamos',
