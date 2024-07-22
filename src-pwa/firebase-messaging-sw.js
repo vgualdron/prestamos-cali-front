cd0a39835d209f13b1cbf532db268582177426f7
vgualdron <victor.gualdron.r@gmail.com>
2024-07-22 02:51:08 -0500

    push new version 33

--- src-pwa/firebase-messaging-sw.js
+++ src-pwa/firebase-messaging-sw.js
@@ -3,6 +3,8 @@ importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js');
 import { precacheAndRoute } from 'workbox-precaching';
 import { clientsClaim } from 'workbox-core';
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 // Configura la precaché y la ruta
 precacheAndRoute(self.__WB_MANIFEST || []);
 
@@ -15,11 +17,34 @@ const firebaseConfig = {
   appId: '1:305627145746:web:e4e5f7808eb9452ef91acb',
   measurementId: 'G-C26N9YLJCL',
 };
+const firebaseApp = initializeApp(firebaseConfig);
+const messaging = getMessaging(firebaseApp);
+Notification.requestPermission().then((permission) => {
+  if (permission === 'granted') {
+    console.log('Notification permission granted.');
+    getToken(messaging, {
+      vapidKey: 'BCNW0_ZWMaTUoLCp-h-oQgYj7OZaX6kbOPeymnZT79sJPNlU5RY7jqqIWSAa4leByqHM_v6Ejrkwkg__htRTRRI',
+    }).then((currentToken) => {
+      if (currentToken) {
+        console.log('FCM Token:', currentToken);
+        // Envía este token a tu servidor para guardar y usar en notificaciones push
+      } else {
+        console.log('No registration token available. Request permission to generate one.');
+      }
+    }).catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+    });
 
-firebase.initializeApp(firebaseConfig);
-
-const messaging = firebase.messaging();
-
+    onMessage(messaging, (payload) => {
+      console.log('Message received. ', payload);
+      // Muestra una notificación usando payload.notification
+    });
+  } else {
+    console.log('Unable to get permission to notify.');
+  }
+}).catch((err) => {
+  console.log('Unable to get permission to notify.', err);
+});
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   const notificationTitle = payload.notification.title;
