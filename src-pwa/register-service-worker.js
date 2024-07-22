0a342497a3b1566e05351bc6666b2bc5daa7ee96
vgualdron <victor.gualdron.r@gmail.com>
2024-07-22 01:15:52 -0500

    push new version 23

--- src-pwa/register-service-worker.js
+++ src-pwa/register-service-worker.js
@@ -8,47 +8,43 @@ console.log('Register service worker ...');
 // events passes a ServiceWorkerRegistration instance in their arguments.
 // ServiceWorkerRegistration: https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration
 
-export default async ({ Vue }) => {
-  register(process.env.SERVICE_WORKER_FILE, {
-    // The registrationOptions object will be passed as the second argument
-    // to ServiceWorkerContainer.register()
-    // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerContainer/register#Parameter
+register(process.env.SERVICE_WORKER_FILE, {
+  // The registrationOptions object will be passed as the second argument
+  // to ServiceWorkerContainer.register()
+  // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerContainer/register#Parameter
 
-    // registrationOptions: { scope: './' },
-    ready(/* registration */) {
-      console.log('Service worker is active');
-    },
-    registered(/* registration */) {
-      console.log('Service worker has been registered.');
-    },
-    cached(/* registration */) {
-      console.log('Content has been cached for offline use.');
-    },
-    updatefound(/* registration */) {
-      console.log('New content is downloading.');
-    },
-    updated(/* registration */) {
-      console.log('New content is available; please refresh.');
-      Notify.create({
-        message: `Hay una versión más reciente que la ${process.env.LATEST_VERSION_APP}, se actualizará automaticamente en un momento`,
-        icon: 'cloud_download',
-        color: 'green',
-        timeout: 5000,
-        textColor: 'white',
-        classes: 'glossy',
-        progress: true,
-        onDismiss() {
-          window.location.reload(true);
-        },
-      });
-    },
-    offline() {
-      console.log('No internet connection found. App is running in offline mode.');
-    },
-    error(err) {
-      console.error('Error during service worker registration:', err);
-    },
-  });
-
-  console.log(Vue);
-};
+  // registrationOptions: { scope: './' },
+  ready(/* registration */) {
+    console.log('Service worker is active');
+  },
+  registered(/* registration */) {
+    console.log('Service worker has been registered.');
+  },
+  cached(/* registration */) {
+    console.log('Content has been cached for offline use.');
+  },
+  updatefound(/* registration */) {
+    console.log('New content is downloading.');
+  },
+  updated(/* registration */) {
+    console.log('New content is available; please refresh.');
+    Notify.create({
+      message: `Hay una versión más reciente que la ${process.env.LATEST_VERSION_APP}, se actualizará automaticamente en un momento`,
+      icon: 'cloud_download',
+      color: 'green',
+      timeout: 5000,
+      textColor: 'white',
+      classes: 'glossy',
+      progress: true,
+      onDismiss() {
+        window.location.reload(true);
+      },
+    });
+  },
+  offline() {
+    console.log('No internet connection found. App is running in offline mode.');
+  },
+  error(err) {
+    console.error('Error during service worker registration:', err);
+  },
+});
