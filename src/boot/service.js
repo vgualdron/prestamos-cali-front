88b7d30e81027f4b5c8867bf75af6c6fb7e75357
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-07-24 00:35:54 -0500

    add version 47

--- src/boot/service.js
+++ src/boot/service.js
@@ -1,9 +1,7 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-        .then((registration) => {
-            console.log('SW -> Service Worker registrado con éxito:', registration);
-        })
-        .catch((error) => {
-            console.error('SW -> Error al registrar el Service Worker:', error);
-        });
+  navigator.serviceWorker.register('/sw.js').then((registration) => {
+    console.log('SW -> Service Worker registrado con éxito:', registration);
+  }).catch((error) => {
+    console.error('SW -> Error al registrar el Service Worker:', error);
+  });
 }
