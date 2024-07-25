5cf4a91bab74286ffd632097bc120758697cd071
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-07-25 16:23:36 -0500

    add version 65

--- src/boot/onesignal.js
+++ src/boot/onesignal.js
@@ -1,5 +1,13 @@
 export default async () => {
   if (process.env.NODE_ENV === 'production') {
+    const loadOneSignalSDK = new Promise((resolve) => {
+      const script = document.createElement('script');
+      script.src = 'https://cdn.onesignal.com/sdks/OneSignalSDK.js';
+      script.async = true;
+      script.onload = resolve;
+      document.head.appendChild(script);
+    });
+    await loadOneSignalSDK;
     window.OneSignal = window.OneSignal || [];
     window.OneSignal.push(() => {
       window.OneSignal.init({
