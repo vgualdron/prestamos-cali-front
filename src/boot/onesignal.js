49dcd6c5a89d64de8280eed7c96eca4d8d7b3251
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-07-26 18:44:35 -0500

    add version 76

--- src/boot/onesignal.js
+++ src/boot/onesignal.js
@@ -12,11 +12,25 @@ export default async () => {
     window.OneSignal.push(async () => {
       await OneSignal.init({
         appId: "da2c1da8-0e9d-4fd0-b66d-522fa6a77841",
-          safari_web_id: "web.onesignal.auto.212e621b-efc2-4b2a-9d36-9f4cd158ecec",
-          notifyButton: {
-            enable: true,
-          },
+        safari_web_id: "web.onesignal.auto.212e621b-efc2-4b2a-9d36-9f4cd158ecec",
+        notifyButton: {
+          enable: true,
+        },
+        promptOptions: {
+          siteName: 'Prestamos Cali',
+          actionMessage: 'Quieres recibir notifiaciones?',
+          exampleNotificationTitle: 'Example Notification Title .ZZZ',
+          exampleNotificationMessage: 'Example Notification Message XXX',
+          exampleNotificationCaption: 'You can unsubscribe at any time YYY',
+        },
       });
+    });
+    window.OneSignal.on('subscriptionChange', (isSubscribed) =>{
+      if (isSubscribed) {
+        OneSignal.getUserId((userId) => {
+          console.log('OneSignal User ID:', userId);
+        });
+      }
     });
   }
 };
