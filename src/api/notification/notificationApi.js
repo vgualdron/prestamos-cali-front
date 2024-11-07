f785064d07f9734cf2856924676a7a6050324bdd
vgualdron <victor.gualdron.r@gmail.com>
2024-11-07 18:54:07 -0500

    add func gest expenses

--- src/api/notification/notificationApi.js
+++ src/api/notification/notificationApi.js
@@ -3,7 +3,7 @@ import { httpPush } from '../../helpers/httpPush';
 export default {
   send: (data) => httpPush({
     headers: {
-      Authorization: 'Basic NDc1MjRjOTUtYjgzNy00OGVjLWJkMjAtYjE1ZGEzODk0OWU4',
+      Authorization: 'Basic NGUyMTFjNDktMTg2Ny00Mzc0LThkMGMtMDY2N2Y4MmFjMGFk',
       'Content-Type': 'application/json',
     },
     url: 'https://onesignal.com/api/v1/notifications',
